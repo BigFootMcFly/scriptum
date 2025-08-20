1dcbdee8988cce4186f02345af9829789740ac6c
Berg János <bergjanos@gmail.com>
2025-08-20 11:06:02 +0200

    + Added seeder for default admin user.

---
+++ config/scriptum.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'setup' => [
+        'admin_email' => env('ADMIN_EMAIL', 'admin@example.com'),
+        'admin_name' => env('ADMIN_NAME', 'Scriptum Admin'),
+        'admin_handle' => env('ADMIN_HANDLE', 'scriptum-admin'),
+        'admin_password' => env('ADMIN_PASSWORD', 'PlEaSe_ChAnGe_Me'),
+    ],
+
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //NOTE: In docker the seeding may be re-run on each restart, so we only create the admin user if it isn't already present
+
+        // skip, if the admin user already exists
+        if (null !== User::where('is_admin', true)->first()) {
+            return;
+        }
+
+        // create the admin user
+        $user = User::create([
+            'name' => config('scriptum.setup.admin_name'),
+            'email' => config('scriptum.setup.admin_email'),
+            'handle' => config('scriptum.setup.admin_handle'),
+            'password' => Hash::make(config('scriptum.setup.admin_password')),
+            'is_admin' => true,
         ]);
+
     }
 }
