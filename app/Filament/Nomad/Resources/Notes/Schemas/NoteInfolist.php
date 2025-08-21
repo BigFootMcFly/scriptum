10a42be5da5d0cc4997e645d93ffe3c887ae7ee9
Berg János <bergjanos@gmail.com>
2025-08-21 17:13:47 +0200

    * Restructured infolist.

--- app/Filament/Nomad/Resources/Notes/Schemas/NoteInfolist.php
+++ app/Filament/Nomad/Resources/Notes/Schemas/NoteInfolist.php
@@ -2,8 +2,12 @@
 
 namespace App\Filament\Nomad\Resources\Notes\Schemas;
 
+use App\Enums\NoteVisibility;
+use Filament\Forms\Components\RichEditor\RichContentRenderer;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\View\Components\BadgeComponent;
 
 class NoteInfolist
 {
@@ -11,19 +15,46 @@ class NoteInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('user.name'),
-                TextEntry::make('visibility'),
-                TextEntry::make('title'),
-                TextEntry::make('slug'),
-                //TODO: make the RichContentRender for the body
-                //TextEntry::make('body'),
-                //TextEntry::make('body_content'),
-                TextEntry::make('deleted_at')
-                    ->dateTime(),
-                TextEntry::make('created_at')
-                    ->dateTime(),
-                TextEntry::make('updated_at')
-                    ->dateTime(),
+                Section::make('Metadata')
+                    //->collapsed(false)
+                    ->collapsible(true)
+                    ->columnSpanFull()
+                    ->columns(2)
+                    ->components([
+                        TextEntry::make('user.name')
+                        ->label('User')
+                    ,
+                    TextEntry::make('visibility')
+                        ->badge()
+                        ->color(fn (NoteVisibility $state): string => match($state) {
+                            NoteVisibility::Private => 'success',
+                            NoteVisibility::Public => 'warning',
+                            NoteVisibility::Restricted => 'danger',
+                            NoteVisibility::Hidden => 'gray',
+                        })
+                    ,
+                    TextEntry::make('title'),
+                    TextEntry::make('slug'),
+                    TextEntry::make('created_at')
+                        ->dateTime(),
+                    TextEntry::make('updated_at')
+                        ->dateTime(),
+                        TextEntry::make('deleted_at')
+                        ->dateTime()
+                        ->placeholder('n/a'),
+
+                    ]),
+                    Section::make('Content')
+                        ->columnSpanFull()
+                        ->columns(1)
+                        ->components([
+                        //TODO: make the RichContentRender for the body
+                        TextEntry::make('body')
+                            ->hiddenLabel()
+                        ,
+                        //TextEntry::make('body_content'),
+                    ])
+
             ]);
     }
 }
