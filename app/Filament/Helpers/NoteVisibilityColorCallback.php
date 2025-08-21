b5b24177ee0f9cd20f69c1ac49442d2796423653
Berg János <bergjanos@gmail.com>
2025-08-21 21:25:36 +0200

    + Added color callback helper.

---
+++ app/Filament/Helpers/NoteVisibilityColorCallback.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Helpers;
+
+use App\Enums\NoteVisibility;
+
+final class NoteVisibilityColors
+{
+
+    public static function make(): callable
+    {
+        return fn (NoteVisibility $state): string => match($state) {
+            NoteVisibility::Private => 'success',
+            NoteVisibility::Public => 'warning',
+            NoteVisibility::Restricted => 'danger',
+            NoteVisibility::Hidden => 'gray',
+        };
+    }
+}
