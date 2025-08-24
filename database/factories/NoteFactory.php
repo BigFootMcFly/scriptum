1ada057d646144499174abd57e0161878a6d7903
Berg János <bergjanos@gmail.com>
2025-08-24 12:30:14 +0200

    * Added a sample 'body' instead of random text.

--- database/factories/NoteFactory.php
+++ database/factories/NoteFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Enums\NoteVisibility;
+use App\Models\Note;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -21,12 +22,14 @@ class NoteFactory extends Factory
     {
         $title = fake()->realText(100);
         $body = fake()->realText();
+        //TODO: make a TipTap JSON faker
+        $body = '{"type":"doc","content":[{"type":"paragraph","content":[{"type":"text","marks":[{"type":"bold"}],"text":"Or"},{"type":"text","text":" is "},{"type":"text","marks":[{"type":"italic"}],"text":"it"},{"type":"text","text":"?"}],"attrs":{"textAlign":"start"}},{"type":"paragraph","attrs":{"textAlign":"start"},"content":[{"type":"text","marks":[{"type":"link","attrs":{"href":"https:\/\/www.google.com\/search?udm=14&q=is+there+a+cow+level%3F","target":"_blank","rel":"noopener noreferrer nofollow","class":null}}],"text":"maybe..."}]},{"type":"paragraph","attrs":{"textAlign":"start"},"content":[{"type":"text","text":"..."}]}]}';
         return [
             'user_id' => User::factory(),
             'title' => $title,
             'slug' => Str::slug($title),
             'body' => $body,
-            'body_content' => Str::limit($body, 20), //TODO: add filter to this
+            'body_content' => Note::extractBodyContents(json_decode($body, true)),
             'visibility' => fake()->randomElement(NoteVisibility::class),
         ];
     }
