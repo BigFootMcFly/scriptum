e9029be1f6db484888a0105e37f1d2f93dbbfeb3
Berg János <bergjanos@gmail.com>
2025-08-29 20:52:39 +0200

    * Added 'refresh-note-list' event handler.

--- app/Filament/User/Resources/Notes/Pages/ListNotes.php
+++ app/Filament/User/Resources/Notes/Pages/ListNotes.php
@@ -5,10 +5,18 @@ namespace App\Filament\User\Resources\Notes\Pages;
 use App\Filament\User\Resources\Notes\NoteResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Livewire\Attributes\On;
 
 class ListNotes extends ListRecords
 {
     protected static string $resource = NoteResource::class;
+
+    #[On('refresh-note-list')]
+    public function refreshPosts(): void
+    {
+        $this->resetPage();
+        //$this->dispatch('$refresh'); // Refreshes the Livewire component
+    }
 
     protected function getHeaderActions(): array
     {
