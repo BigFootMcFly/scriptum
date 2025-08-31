5c2bfbe5efae78f6df2a5e632c4b279e872497ea
Berg János <bergjanos@gmail.com>
2025-08-31 17:55:40 +0200

    + Added custom logout controller and action.

---
+++ app/Actions/LogoutUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Actions;
+
+use Filament\Facades\Filament;
+
+final class LogoutUser
+{
+
+    public static function execute(?string $redirectTo = null): void
+    {
+        Filament::auth()->logout();
+
+        session()->invalidate();
+        session()->regenerateToken();
+
+        if ($redirectTo === null) {
+            $redirectTo = 'filament.user.resources.notes.index';
+        }
+    }
+
+}
---
+++ app/Http/Controllers/LogoutUserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Actions\LogoutUser;
+use Illuminate\Http\Request;
+
+class LogoutUserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        LogoutUser::execute();
+        return redirect()->to(route('filament.user.pages.main-page'));
+    }
+}
