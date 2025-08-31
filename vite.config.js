6bd98f9b1f010bbb582d74a57b40c0b89ba84296
Berg János <bergjanos@gmail.com>
2025-08-31 17:50:02 +0200

    + Added User panel its own theme.

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/css/filament/nomad/theme.css'
+                'resources/css/filament/nomad/theme.css',
+                'resources/css/filament/user/theme.css'
             ],
             refresh: true,
         }),
