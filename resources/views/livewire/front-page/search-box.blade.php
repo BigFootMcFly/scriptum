bea56ed6b7e85ae39ebad8a74601bb72abf52658
Berg János <bergjanos@gmail.com>
2025-09-01 22:32:12 +0200

    * Updated search bar width to fill available space.

--- resources/views/livewire/front-page/search-box.blade.php
+++ resources/views/livewire/front-page/search-box.blade.php
@@ -1,6 +1,6 @@
-<div class="w-auto" x-on:keydown.window.ctrl.shift.f.prevent="$refs.searchInput.focus()">
+<div class="flex-1" x-on:keydown.window.ctrl.shift.f.prevent="$refs.searchInput.focus()">
 </script>
-    <div class="fi-input-wrp w-7xl">
+    <div class="fi-input-wrp w-full mx-auto">
         <div class="fi-input-wrp-prefix fi-input-wrp-prefix-has-content fi-inline">
             <svg wire:loading.remove.delay.default="1" wire:target="search" class="fi-icon fi-size-md" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                 <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 1 0 0 11 5.5 5.5 0 0 0 0-11ZM2 9a7 7 0 1 1 12.452 4.391l3.328 3.329a.75.75 0 1 1-1.06 1.06l-3.329-3.328A7 7 0 0 1 2 9Z" clip-rule="evenodd"></path>
