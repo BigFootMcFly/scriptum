e6da0d1d135dfb28b66e0ecdb5a0fe9e5f950ce0
Berg János <bergjanos@gmail.com>
2025-09-03 09:14:57 +0200

    * Added check for guest user.

--- app/Filament/User/Resources/Notes/NoteResource.php
+++ app/Filament/User/Resources/Notes/NoteResource.php
@@ -44,8 +44,12 @@ class NoteResource extends Resource
 
     public static function getEloquentQuery(): Builder
     {
-        $query = parent::getEloquentQuery()->where('user_id', auth()->user()->id);
 
+        $query = parent::getEloquentQuery();
+
+        if (auth()->check()) {
+            $query->where('user_id', auth()->user()->id);
+        }
 
         //NOTE: add this if the resource table should be filtered by the top search as well...
         $forntPageSearch = session('front-page-search','');
