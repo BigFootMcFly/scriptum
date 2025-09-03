9cb669ef9e4a025308e52c27a9a27ac63b2db94b
Berg János <bergjanos@gmail.com>
2025-09-03 13:38:51 +0200

    * Tood completed (added email_verified badge),

--- app/Filament/Nomad/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Nomad/Resources/Users/Tables/UsersTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Nomad\Resources\Users\Tables;
 
+use App\Models\User;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
@@ -15,6 +16,10 @@ class UsersTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->persistSortInSession()
+            ->reorderableColumns()
+            //->persistSearchInSession()
+            //->persistColumnSearchesInSession()
             ->columns([
                 TextColumn::make('name')
                     ->searchable(),
@@ -23,11 +28,20 @@ class UsersTable
                 TextColumn::make('email')
                     ->label('Email address')
                     ->searchable(),
+                IconColumn::make('verified')
+                    ->state( fn(User $record) => $record->isVerified())
+                    ->label('Verified')
+                    ->sortable(['email_verified_at'])
+                    ->boolean()
+                    ->tooltip(fn (User $record) => $record->email_verified_at)
+                    ,
+                IconColumn::make('is_admin')
+                    ->label('Admin')
+                    ->boolean(),
                 TextColumn::make('email_verified_at')
                     ->dateTime()
-                    ->sortable(),
-                IconColumn::make('is_admin')
-                    ->boolean(),
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
                 TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
