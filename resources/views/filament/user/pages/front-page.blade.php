fc342d242f847f8d5bfe228216f8b184631f6b0c
Berg János <bergjanos@gmail.com>
2025-09-06 10:36:02 +0200

    * Code cleanup.

--- resources/views/filament/user/pages/front-page.blade.php
+++ resources/views/filament/user/pages/front-page.blade.php
@@ -1,7 +1,9 @@
 <x-filament-panels::page>
+    {{-- Common components --}}
     <livewire:edit-note-modal-form></livewire:edit-note-modal-form>
     <livewire:front-page.back-to-top-button></livewire:front-page.back-to-top-button>
 
+    {{-- Note list --}}
     <div class="divide-y divide-gray-200 dark:divide-gray-800">
         @foreach ($this->notes() as $note)
             <livewire:view-note.note :note="$note" :key="$note->id"></livewire:view-note.note>
@@ -12,5 +14,4 @@
     <div class="mt-6">
         <x-filament::pagination :paginator="$this->notes()" />
     </div>
-
 </x-filament-panels::page>
