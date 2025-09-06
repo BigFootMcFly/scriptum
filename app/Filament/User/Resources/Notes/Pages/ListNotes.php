ce813592bf118ece4e0c25f1a367ec0b83b98d26
Berg János <bergjanos@gmail.com>
2025-09-06 20:23:12 +0200

    * Added tabs to notes list table.

--- app/Filament/User/Resources/Notes/Pages/ListNotes.php
+++ app/Filament/User/Resources/Notes/Pages/ListNotes.php
@@ -5,6 +5,8 @@ namespace App\Filament\User\Resources\Notes\Pages;
 use App\Filament\User\Resources\Notes\NoteResource;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Attributes\On;
 
 class ListNotes extends ListRecords
@@ -17,6 +19,27 @@ class ListNotes extends ListRecords
         $this->resetPage();
         //$this->dispatch('$refresh'); // Refreshes the Livewire component
     }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge(auth()->user()->notes()->withTrashed()->count())
+                ->badgeColor('info'),
+            'public' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->public())
+                ->badge(auth()->user()->notes()->withTrashed()->public()->count()),
+            'private' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->private())
+                ->badge(auth()->user()->notes()->withTrashed()->private()->count())
+                ->badgeColor('success'),
+            'deleted' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->onlyTrashed())
+                ->badge(auth()->user()->notes()->onlyTrashed()->count())
+                ->badgeColor('danger'),
+        ];
+    }
+
 
     protected function getHeaderActions(): array
     {
