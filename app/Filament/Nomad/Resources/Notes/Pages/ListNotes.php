29a25bc8b518a6b453231c5e00279915a50a9fe1
Berg János <bergjanos@gmail.com>
2025-09-06 18:58:04 +0200

    * Added tabs to the notes table view.

--- app/Filament/Nomad/Resources/Notes/Pages/ListNotes.php
+++ app/Filament/Nomad/Resources/Notes/Pages/ListNotes.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\Nomad\Resources\Notes\Pages;
 
+use App\Enums\NoteVisibility;
 use App\Filament\Nomad\Resources\Notes\NoteResource;
+use App\Models\Note;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Filament\Schemas\Components\Tabs\Tab;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListNotes extends ListRecords
 {
@@ -16,6 +20,35 @@ class ListNotes extends ListRecords
             CreateAction::make(),
         ];
     }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make()
+                ->badge(Note::withTrashed()->count())
+                ->badgeColor('info'),
+            'deleted' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('deleted_at', '<>', null))
+                ->badge(Note::onlyTrashed()->count())
+                ->badgeColor('danger'),
+            'public' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->withoutTrashed()->where('visibility', NoteVisibility::Public))
+                ->badge(Note::withoutTrashed()->where('visibility', NoteVisibility::Public)->count()),
+            'private' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->withoutTrashed()->where('visibility', NoteVisibility::Private))
+                ->badge(Note::withoutTrashed()->where('visibility', NoteVisibility::Private)->count())
+                ->badgeColor('success'),
+            'hidden' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->withoutTrashed()->where('visibility', NoteVisibility::Hidden))
+                ->badge(Note::withoutTrashed()->where('visibility', NoteVisibility::Hidden)->count())
+                ->badgeColor('danger'),
+            'restricted' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->withoutTrashed()->where('visibility', NoteVisibility::Restricted))
+                ->badge(Note::withoutTrashed()->where('visibility', NoteVisibility::Restricted)->count())
+                ->badgeColor('danger'),
+        ];
+    }
+
 /*
     public function getTitle(): string
     {
