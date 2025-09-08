5309a980a46f828b83e6a88eb3d2e57dd4b70ff9
Berg János <bergjanos@gmail.com>
2025-09-08 20:41:57 +0200

    * Updated event handlers.

--- app/Livewire/FrontPage/NewNoteButton.php
+++ app/Livewire/FrontPage/NewNoteButton.php
@@ -10,16 +10,9 @@ use Livewire\Component;
 class NewNoteButton extends Component
 {
 
-    #[On('refresh-new-note-button')]
-    public function onNavigated(string $pathName) {
-        /*
-        //NOTE: for now, the disabled button is not used anywhere, this functionality will propably be removed later, if teher could no new use for it be found
-        //      the event may be remaining here, in case a refres is needed, but could be removed as well...
-        $frontPagePath =  parse_url(FrontPage::getNavigationUrl(),  PHP_URL_PATH);
-        $manageMyNotesPath = parse_url(ListNotes::getNavigationUrl(),  PHP_URL_PATH);
-        //$this->disabled = $manageMyNotesPath === $pathName;
-        $this->disabled = false;
-        */
+    #[On('spa-navigation')]
+    public function onSpaNavigation(string $pathName): void {
+        // add changees here if needed
     }
 
     public function render()
