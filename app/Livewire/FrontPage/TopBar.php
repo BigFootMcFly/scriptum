176c7e82259b47db1c2309be8d5bf65119820d64
Berg János <bergjanos@gmail.com>
2025-09-10 11:33:05 +0200

    * Changed render mode from button to list item and added profile action.

--- app/Livewire/FrontPage/TopBar.php
+++ app/Livewire/FrontPage/TopBar.php
@@ -11,6 +11,7 @@ use Filament\Panel\Concerns\HasUserMenu;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
 use Filament\Support\Concerns\EvaluatesClosures;
+use Filament\Support\Icons\Heroicon;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
@@ -24,27 +25,40 @@ class TopBar extends Component
     public function refresh(): void {}
 
     public function mount() {
+
         $this->userMenuItems([
+            Action::make('profile')
+                ->label(__('Profile'))
+                ->icon('heroicon-o-user-circle')
+                ->url(route('filament.user.pages.edit-profile'))
+                ->sort(-1)
+                ->defaultView(Action::GROUPED_VIEW)
+            ,
             Action::make('login')
                 ->label(__('Login / Register'))
                 ->icon('heroicon-o-user-circle')
                 ->url(route('filament.user.auth.login'))
                 ->sort(1)
+                ->defaultView(Action::GROUPED_VIEW)
                 ->visible( fn (): bool => !auth()->check())
                 ,
-            //NOTE: this will tak precedence over the default 'logouz' action defined by filament
+            //NOTE: this will take precedence over the default 'logouz' action defined by filament
             Action::make('logout')
                 ->label(__('Logout'))
                 ->icon('heroicon-o-arrow-left-on-rectangle')
                 ->url(route('logout-user'))
                 ->postToUrl()
                 ->sort(PHP_INT_MAX)
+                ->defaultView(Action::GROUPED_VIEW)
         ]);
+
     }
 
-
+/*
     public function render()
     {
         return view('livewire.front-page.top-bar');
     }
+*/
+
 }
