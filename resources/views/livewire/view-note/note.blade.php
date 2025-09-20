dfc043355323c0ddd08ff9a248a1e13b623ea351
Berg János <bergjanos@gmail.com>
2025-09-20 11:05:09 +0200

    * Updated views to be more usable on mobile phones.

--- resources/views/livewire/view-note/note.blade.php
+++ resources/views/livewire/view-note/note.blade.php
@@ -1,7 +1,8 @@
 <div @class([
-        "divide-y divide-gray-200 dark:divide-gray-800  divide-double",
+        "divide-y divide-gray-200 dark:divide-gray-800 divide-double sm:px-3 lg:px-5",
         "animate-note-updated" => $pulse,
     ])
+    class=""
 >
     <div class="py-4">
         {{-- Header --}}
@@ -16,27 +17,24 @@
                 >
                     {{ $note->user->handle }}
                 </a>
+                @if (auth()?->user()?->can('updateOnFrontPage', $note))
+                    {{-- Edit button --}}
+                    <x-filament::icon-button
+                        icon="heroicon-m-pencil-square"
+                        tag="a"
+                        wire:click="$dispatch('edit-note', { note: {{ $note->id }} })"
+                        size="xs"
+                        color="warning"
+                        label="Edit"
+                        tooltip="Edit Note"
+                        class="grayscale hover:grayscale-0 duration-300 cursor-pointer float-end"
+                    />
+                @endif
             </div>
             <div class="flex justify-between items-start">
                 <h1 class="font-semibold text-gray-900 dark:text-gray-100 text-2xl">
                     {{ $note->title }}
                 </h1>
-
-
-                @if (auth()?->user()?->can('updateOnFrontPage', $note))
-
-                {{-- Edit button --}}
-                <x-filament::icon-button
-                    icon="heroicon-m-pencil-square"
-                    tag="a"
-                    wire:click="$dispatch('edit-note', { note: {{ $note->id }} })"
-                    size="sm"
-                    color="warning"
-                    label="Edit"
-                    tooltip="Edit Note"
-                    class="grayscale hover:grayscale-0 duration-300 cursor-pointer"
-                />
-                @endif
             </div>
         </div>
 
@@ -52,9 +50,8 @@
                 fi-prose
                 rounded-xl
                 border-0
-                border-sky-950
+                dark:border-zinc-800 border-zink-100
                 p-2
-
                 ">
                 {!! $note->renderRichContent('body') !!}
             </div>
@@ -62,7 +59,9 @@
 
         {{-- Footer --}}
         <div class="flex justify-between items-start py-2">
-            <span class="mt-2 text-xs text-gray-500 dark:text-gray-700">{{ $note->created_at}}</span>
+            <span class="mt-2 text-xs text-gray-500 dark:text-gray-600">
+                {{ $note->created_at}}
+            </span>
             <div class="mt-2 text-xs text-gray-500 dark:text-gray-400">
                 <x-filament::link
                     icon="heroicon-m-arrow-top-right-on-square"
