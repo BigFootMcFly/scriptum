e770c47c4cd6b913585b6f76e6f1644068411d95
Berg János <bergjanos@gmail.com>
2025-10-13 11:43:50 +0200

    * Updated entrypoint.

--- build/nginx/entrypoint.sh
+++ build/nginx/entrypoint.sh
@@ -35,11 +35,6 @@ function create_storage_folders {
 # Initalizing caches
 function initalize_cache {
 
-    composer install \
-        --no-interaction \
-        --no-dev \
-        --optimize-autoloader
-
     # populating/updating caches
     php artisan optimize \
     && php artisan filament:cache-components || {
