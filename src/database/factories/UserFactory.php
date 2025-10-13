035151ce91aa61aa5f63d3b30b24d1cdbbe8f76b
Berg János <bergjanos@gmail.com>
2025-10-13 14:57:53 +0200

    * Updated factory after MFA added.

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -36,6 +36,7 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
             'viewing_mode' => FrontPageViewingMode::Private,
+            'has_email_authentication' => false,
         ];
     }
 
