5680cd65e62657e8aa583ceff0a266b06fbefe5d
Berg János <bergjanos@gmail.com>
2025-10-14 13:15:49 +0200

    * Updated trusted proxys filter.

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -15,9 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'is_admin' => IsAdmin::class
         ]);
-        $middleware->trustProxies(at: [
-            '*',
-        ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
