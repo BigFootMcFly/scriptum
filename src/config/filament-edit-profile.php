79b7fcca7a8601e8a79d1fd739e93f8c9b40a4f9
Berg János <bergjanos@gmail.com>
2025-10-16 08:09:34 +0200

    * Changed back visibility to the default.

--- src/config/filament-edit-profile.php
+++ src/config/filament-edit-profile.php
@@ -4,8 +4,7 @@ return [
     'locale_column' => 'locale',
     'theme_color_column' => 'theme_color',
     'avatar_column' => 'avatar_url',
-    //'disk' => env('FILESYSTEM_DISK', 'public'),
-    'disk' =>  'public',
+    'disk' => env('FILESYSTEM_DISK', 'public'),
     'visibility' => 'public', // or replace by filesystem disk visibility with fallback value
 //    'visibility' => 'public', // or replace by filesystem disk visibility with fallback value
 ];
