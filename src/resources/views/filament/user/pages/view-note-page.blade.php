02b41a31da677b60401982a10a94f16202fffdab
Berg János <bergjanos@gmail.com>
2025-10-16 09:39:57 +0200

    * Removed footer.

--- src/resources/views/filament/user/pages/view-note-page.blade.php
+++ src/resources/views/filament/user/pages/view-note-page.blade.php
@@ -1,5 +1,4 @@
 <x-filament-panels::page>
     <livewire:front-page.back-to-top-button></livewire:front-page.back-to-top-button>
     @livewire('view-note.note', ['note' => $note])
-    <livewire:page-footer></livewire:page-footer>
 </x-filament-panels::page>
