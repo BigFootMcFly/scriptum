eb80d2a1e910314ebcf83dfe8090835f1e2391cd
Berg János <bergjanos@gmail.com>
2025-10-18 21:09:30 +0200

    * Replaced previously deleted component (left in by mistake).

--- src/resources/views/filament/user/pages/view-note-page.blade.php
+++ src/resources/views/filament/user/pages/view-note-page.blade.php
@@ -1,5 +1,5 @@
 <x-filament-panels::page>
     <livewire:front-page.back-to-top-button></livewire:front-page.back-to-top-button>
     @livewire('view-note.note', ['note' => $note])
-    <livewire:edit-note-modal-form></livewire:edit-note-modal-form>
+    {{ $this->getModalView() }}
 </x-filament-panels::page>
